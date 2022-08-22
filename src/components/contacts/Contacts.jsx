5d5327f005e9ec1669e1f554726807917533d42a
Arbie Abesamis <arbieabesamis@gmail.com>
2022-08-22 21:42:23 +0800

    contact page

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -13,27 +13,23 @@ export const Contacts = () => {
           <img src={logo} />
           <p>Tel: 912-345-6789</p>
           <p>Tel: 0123-4567</p>
-          <p>Email: magsautorepairshop@hotmail.com</p>
+          <p>Email: magsautorepairshop@hotmail.com.</p>
+          <p>
+            <address>Seaside Blvd, 123, Pasay, 1300 Metro Manila</address>
+          </p>
         </div>
-        {/* <div className="contacts-map_info">
-          <div class="mapouter">
-            <div class="gmap_canvas">
-              <iframe
-                width="600"
-                height="500"
-                id="gmap_canvas"
-                src="https://maps.google.com/maps?q=mall%20of%20asia&t=&z=15&ie=UTF8&iwloc=&output=embed"
-                frameborder="0"
-                scrolling="no"
-                marginheight="0"
-                marginwidth="0"
-              ></iframe>
-              <a href="https://123movies-to.org">123movies</a>
-              <br />
-              <a href="https://www.embedgooglemap.net">google map embed</a>
-            </div>
-          </div>
-        </div> */}
+        <div className="contacts-map_info">
+          <iframe
+            width="600"
+            height="500"
+            id="gmap_canvas"
+            src="https://maps.google.com/maps?q=mall%20of%20asia&t=&z=15&ie=UTF8&iwloc=&output=embed"
+            frameborder="0"
+            scrolling="no"
+            marginheight="0"
+            marginwidth="0"
+          ></iframe>
+        </div>
       </div>
     </div>
   );
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,8 +5,8 @@ import logo from "../../assets/MagsLogoWhite.png";
 export const Footer = () => {
   return (
     <div className="footer">
-      <div className="container">
-        <div className="row">
+      <div className="container-footer">
+        <div className="row-item">
           <div className="footer-col">
             <h4>COMPANY INFORMATION</h4>
             <ul className="comp">
@@ -17,7 +17,7 @@ export const Footer = () => {
                 <a href="#services">Services</a>
               </li>
               <li>
-                <a href="#">Company Policy</a>
+                <a href="#home">Company Policy</a>
               </li>
             </ul>
           </div>
@@ -26,13 +26,13 @@ export const Footer = () => {
             <h4>ONLINE SUPPORT </h4>
             <ul className="support">
               <li>
-                <a href="#">Customer care </a>
+                <a href="#contacts">Customer care </a>
               </li>
               <li>
-                <a href="#">Chat </a>
+                <a href="#home">Chat </a>
               </li>
               <li>
-                <a href="#">Email Add </a>
+                <a href="#contacts">Email Add </a>
               </li>
             </ul>
           </div>
@@ -41,13 +41,13 @@ export const Footer = () => {
             <h4>CONTACT US </h4>
             <ul className="contact">
               <li>
-                <a href="">45455</a>
+                <a href="tel:555-666-7777">45455</a>
               </li>
               <li>
-                <a href="">0475586249</a>
+                <a href="tel:555-666-7777">0475586249</a>
               </li>
               <li>
-                <a href="">1531 addresss</a>
+                <a href="tel:555-666-7777">1531 addresss</a>
               </li>
             </ul>
           </div>
@@ -56,20 +56,20 @@ export const Footer = () => {
       <div className="brand-links">
         <div className="mags-logo">
           <a href="#home">
-            <img src={logo} />
+            <img src={logo} alt="logo" />
           </a>
         </div>
         <div class="social-links">
-          <a href="#">
+          <a href="https://www.facebook.com/">
             <i class="fa-brands fa-facebook-f"></i>
           </a>
-          <a href="#">
+          <a href="https://www.twitter.com/">
             <i class="fa-brands fa-twitter"></i>
           </a>
-          <a href="#">
+          <a href="https://www.instagram.com/">
             <i class="fa-brands fa-instagram"></i>
           </a>
-          <a href="#">
+          <a href="https://www.linkedin.com/">
             <i class="fa-brands fa-linkedin-in"></i>
           </a>
         </div>
